2edf631839640706e138de69f8de54da4c25179f
simasturbo07 <161766609+simasturbo07@users.noreply.github.com>
2025-04-16 11:40:10 +0000

    1000%atlz

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
     const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade');
-    const opcoesAcessibilidade = document.getElementById('botao-acessibilidade');
+    const opcoesAcessibilidade = document.getElementById('opcoes-acessibilidade');
     botaoDeAcessibilidade.addEventListener('click', function(){
         botaoDeAcessibilidade.classList.toggle('rotacao-botao')
         opcoesAcessibilidade.classList.toggle('apresenta-lista')
